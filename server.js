e662638d97272f9979a9b083a2302b41020da378
deepak-2605 <108828387+deepak-2605@users.noreply.github.com>
2022-07-19 15:08:28 +0530

    Hello again

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const peerServer = ExpressPeerServer(server, {
   debug: true
 });
 app.set('view engine','ejs');
+app.use('/peerjs', peerServer);
 app.use(express.static('public'));
 let a=uuidV4();
 app.get('/',(req,res)=>{
