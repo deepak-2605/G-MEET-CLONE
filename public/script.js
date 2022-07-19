273fd5838d0dac488425d68fb3ad241f0171cf7f
ADITYAGUPTA118 <adityagupta200317@gmail.com>
2022-07-19 16:58:00 +0530

    buttons and chat-box

--- public/script.js
+++ public/script.js
@@ -140,7 +140,7 @@ const setMuteButton=()=>{
     // console.log('setMuted');
     const html=`
     <i class="fas fa-microphone"></i>
-                    <span>Mute</span>
+                    
     `;
     console.log(html);
     document.querySelector('.main__mute_button').innerHTML=html;
@@ -148,7 +148,7 @@ const setMuteButton=()=>{
 const setUnmuteButton=()=>{
     const html=`
     <i class="fas fa-microphone-lines-slash"></i>
-                    <span>Unmute</span>
+                    
     `
     console.log(html);
     document.querySelector('.main__mute_button').innerHTML=html;
@@ -156,14 +156,14 @@ const setUnmuteButton=()=>{
 const setPlayVideo = () => {
     const html = `
     <i class="stop fas fa-video-slash"></i>
-      <span>Play Video</span>
+    
     `
     document.querySelector('.main__video_button').innerHTML = html;
   }
   const setStopVideo = () => {
     const html = `
       <i class="fas fa-video"></i>
-      <span>Stop Video</span>
+     
     `
     document.querySelector('.main__video_button').innerHTML = html;
   }
